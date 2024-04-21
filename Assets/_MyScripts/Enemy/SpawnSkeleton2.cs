5906426288ab4af8d834f2f1270901524dd537e7
QuocHung3 <86833306+QuocHung3@users.noreply.github.com>
2024-04-21 22:17:30 +0700

    finish skeleton 2

--- Assets/_MyScripts/Enemy/SpawnSkeleton2.cs
+++ Assets/_MyScripts/Enemy/SpawnSkeleton2.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class SpawnSkeleton2 : MonoBehaviour
+public class SpawnSkeleton2 : Spawner
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject playerGameObject;
+    void Reset()
     {
-        
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        this.Refab = Resources.Load<GameObject>("Skeleton2");
+        this.Parent = null;
+        this.spawnTime = 2.0f;
+        this.spawnCount = 5;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!playerGameObject) playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        this.Timer();
+        RandomPosition();
+        SpawnRefabsInTimerForCount();
+    }
+    protected void RandomPosition()
+    {
+        float random = Random.Range(-0.5f, 0.5f) > 0 ? Random.Range(5f, 8f) : Random.Range(-5f, -8f);
+        Vector3 playerPosition = playerGameObject.transform.position;
+        this.positionSpawn = new Vector3(playerPosition.x + random, playerPosition.y + random, 0);
     }
 }
