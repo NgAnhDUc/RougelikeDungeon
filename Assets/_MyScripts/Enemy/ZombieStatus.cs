0495f769a72bf34298f9b303f55304ce025a9194
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2024-04-21 17:22:24 +0700

    fix Photon Spawn Zombie

--- Assets/_MyScripts/Enemy/ZombieStatus.cs
+++ Assets/_MyScripts/Enemy/ZombieStatus.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ZombieStatus : MonoBehaviour
+using Photon.Pun;
+public class ZombieStatus : MonoBehaviourPun
 {
      public float heath = 10f;
      public float strength = 10f;
