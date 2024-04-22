c0696772475f5a4cd73d9528bc9804f7566054ad
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2024-04-22 11:12:52 +0700

    player card spawn

--- Assets/_MyScripts/MenuStart/AutoSetPlayerCard.cs
+++ Assets/_MyScripts/MenuStart/AutoSetPlayerCard.cs
@@ -2,11 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Photon.Pun;
 
-public class AutoSetPlayerCard : MonoBehaviour
+public class AutoSetPlayerCard : MonoBehaviourPun
 {
-    void Start()
+    TMP_Text heroNameText;
+    private void Start()
     {
         transform.localScale = new Vector3(1, 1, 1);
+        Transform heroName = transform.Find("Player Name");
+        this.heroNameText = heroName.GetComponent<TMP_Text>();
+        
+    }
+    private void Update()
+    {
+        if (this.photonView.ViewID == 0) return;
+        this.heroNameText.text = this.photonView.Owner.NickName;
     }
 }
